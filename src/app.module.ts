518b538fb420fbaae1fe72dc2dd00d6fb382cee1
Do Nguyen Hoang Minh Chien <chien.do@saigontechnology.com>
2022-03-11 16:00:05 +0700

    implement auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import {LoggerMiddleware } from './shared/Middleware/logger.middleware';
+import { AuthModule } from './auth/auth.module';
+import { AuthModule } from './auth/auth.module';
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
