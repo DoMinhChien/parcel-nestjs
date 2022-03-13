19db6928a37613f65ec3d5c73d5f3f809ac5191e
Do Nguyen Hoang Minh Chien <chien.do@saigontechnology.com>
2022-03-13 17:28:58 +0700

    update displayname

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,15 +2,26 @@ import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
-
-import {LoggerMiddleware } from './shared/Middleware/logger.middleware';
-
-
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from '@hapi/joi';
+import { DatabaseModule } from './database/database.module';
 import {LoggerMiddleware } from './shared/middleware/logger.middleware';
->>>>>>> 7f1a2c8947f8a673b45a1aebb35fbef2150cc69f
 import { AuthModule } from './auth/auth.module';
 @Module({
-  imports: [UserModule, AuthModule],
+  imports: [UserModule,
+     AuthModule,
+     ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        POSTGRES_HOST: Joi.string().required(),
+        POSTGRES_PORT: Joi.number().required(),
+        POSTGRES_USER: Joi.string().required(),
+        POSTGRES_PASSWORD: Joi.string().required(),
+        POSTGRES_DB: Joi.string().required(),
+        PORT: Joi.number(),
+      })
+    }),
+    DatabaseModule,
+    ],
   controllers: [AppController],
   providers: [AppService],
 })
