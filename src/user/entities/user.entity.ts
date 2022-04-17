0585afcdf219222c5ce07752d90715d3dfcb788e
MikeYuen <mike.dev@instajs.com>
2022-04-17 19:35:48 +0700

    nullable address & location for user entity

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,5 +1,13 @@
 import { OrderEntity } from '../../order/entities/order.entity';
-import { Entity, Column, PrimaryColumn, OneToMany, ManyToMany, OneToOne, JoinTable } from 'typeorm';
+import {
+  Entity,
+  Column,
+  PrimaryColumn,
+  OneToMany,
+  ManyToMany,
+  OneToOne,
+  JoinTable,
+} from 'typeorm';
 import { Exclude } from 'class-transformer';
 import { DriverEntity } from '../../driver/entities/driver.entity';
 import { RoleEntity } from './role.entity';
@@ -11,30 +19,39 @@ export class UserEntity {
 
   @Column('varchar', { length: 500 })
   email: string;
+
   @Column('varchar', { length: 64 })
   displayName: string;
+
   @Column('varchar', { length: 500 })
   password: string;
-  @Column('varchar', { length: 64 })
-  address: string;
+
   @Column('varchar', { length: 64 })
   phone: string;
-  @Column('varchar', {})
+
+  @Column('varchar', { length: 64, nullable: true })
+  address: string;
+
+  @Column('varchar', { nullable: true })
   provinceCode: string;
+
+  @Column('varchar', { length: 100, nullable: true })
+  location: string;
+
   @Column('int', { default: 0 })
   gender: number;
+
   @OneToMany(() => OrderEntity, (order) => order.user)
   orders: OrderEntity[];
+
   @Column({ nullable: true })
   @Exclude()
   public currentHashedRefreshToken?: string;
 
-  @OneToOne(() => DriverEntity, driver => driver.user) // specify inverse side as a second parameter
+  @OneToOne(() => DriverEntity, (driver) => driver.user) // specify inverse side as a second parameter
   driver: DriverEntity;
 
-  @ManyToMany(() => RoleEntity, (role : RoleEntity) => role.users)
+  @ManyToMany(() => RoleEntity, (role: RoleEntity) => role.users)
   @JoinTable()
-  public roles : RoleEntity[];
-  @Column('varchar', {length: 100, nullable: true}, )
-  location: string;
+  public roles: RoleEntity[];
 }
