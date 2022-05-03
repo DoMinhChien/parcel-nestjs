c6052dc07a74735487f6d4be56e8dd4561abedaf
MikeYuen <mike.dev@instajs.com>
2022-05-03 10:20:56 +0700

    remove nonsense log

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -15,7 +15,6 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromExtractors([
         (request: Request) => {
-          console.log(request);
           return request?.headers?.authorization;
         },
       ]),
